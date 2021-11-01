521e0626d5ab8cd808437b8408a518e54866fd60
jmcorbera <jmcorbera@gmail.com>
2021-11-01 17:22:55 -0300

    Update ScriptASM.sh

--- ScriptASM.sh
+++ ScriptASM.sh
@@ -1,3 +1,5 @@
 sudo nasm -f elf32 ./Enmascarado_ASM/enmascarar_asm.asm -o ./Enmascarado_ASM/enmascarar_asm.o;
 sudo gcc -m32 ./Enmascarado_ASM/enmascarar_asm.c ./Enmascarado_ASM/enmascarar_asm.o -o maskASM;
-#./maskASM "avengers.bmp" "ligaj.bmp" "mascara.bmp" 1920 1080
+./maskASM "avengers.bmp" "ligaj.bmp" "mascara.bmp" 1920 1080
+./maskASM "dragonball300x242.bmp" "codegeass.bmp" "mascara300x242.bmp" 300 242
+./maskASM "onepiece1024x768.bmp" "gogeta1024x768.bmp" "mascara1024x768.bmp" 1024 768
