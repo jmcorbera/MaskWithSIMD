c0949b2a2b372d3e1151b515d4d0ee8f28417449
jmcorbera <jmcorbera@gmail.com>
2021-11-01 17:14:22 -0300

    Update ScriptC.sh

--- ScriptC.sh
+++ ScriptC.sh
@@ -1,2 +1,4 @@
 sudo gcc -m32 ./Enmascarado_C/enmascarar_c.c -o maskC;
-#./mask_C "avengers.bmp" "ligaj.bmp" "mascara.bmp" 1920 1080
+./maskC "avengers.bmp" "ligaj.bmp" "mascara.bmp" 1920 1080
+./maskC "dragonball300x242.bmp" "codegeass.bmp" "mascara300x242.bmp" 300 242
+./maskC "onepiece1024x768.bmp" "gogeta1024x768.bmp" "mascara1024x768.bmp" 1024 768
