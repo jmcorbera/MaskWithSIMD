8b5f2acdc875d92b4e581a8f4a4b6c208a929c07
jmcorbera <jmcorbera@gmail.com>
2021-11-01 17:52:43 -0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,10 @@
-sudo nasm -f elf32 enmascarar_asm.asm -o enmascarar_asm.o;
-sudo gcc -m32 enmascarar_c.c enmascarar_asm.o -o maskSIMD;
-./maskSIMD "avengers.bmp" "ligaj.bmp" "mascara.bmp" 1920 1080
-./maskSIMD "dragonball300x242.bmp" "codegeass.bmp" "mascara300x242.bmp" 300 242
-./maskSIMD "onepiece1024x768.bmp" "gogeta1024x768.bmp" "mascara1024x768.bmp" 1024 768
+sudo gcc -m32 ./Enmascarado_C/enmascarar_c.c -o maskC;
+sudo nasm -f elf32 ./Enmascarado_ASM/enmascarar_asm.asm -o ./Enmascarado_ASM/enmascarar_asm.o;
+sudo gcc -m32 ./Enmascarado_ASM/enmascarar_asm.c ./Enmascarado_ASM/enmascarar_asm.o -o maskASM;
+./maskC "avengers.bmp" "ligaj.bmp" "mascara.bmp" 1920 1080
+./maskC "dragonball300x242.bmp" "codegeass.bmp" "mascara300x242.bmp" 300 242
+./maskC "onepiece1024x768.bmp" "gogeta1024x768.bmp" "mascara1024x768.bmp" 1024 768
+./maskASM "avengers.bmp" "ligaj.bmp" "mascara.bmp" 1920 1080
+./maskASM "dragonball300x242.bmp" "codegeass.bmp" "mascara300x242.bmp" 300 242
+./maskASM "onepiece1024x768.bmp" "gogeta1024x768.bmp" "mascara1024x768.bmp" 1024 768
 
